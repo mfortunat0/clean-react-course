110cfc5f8061499a2d47f83b937bbd895205ccba
matheus <matheus.xmaz10@gmail.com>
2021-05-24 21:29:14 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,36 @@ module.exports = {
     filename: "bundle.js",
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: [".ts", ".tsx", ".js", "scss"],
     alias: {
       "@": path.join(__dirname, "src"),
     },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "sass-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              module: true,
+            },
+          },
+          {
+            loader: "style-loader",
+          },
+        ],
+      },
+    ],
   },
   devServer: {
     contentBase: "./public",

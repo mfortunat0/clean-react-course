86beb9a966c1a1fbcf5e8b65779c2af5847114bc
matheus <matheus.xmaz10@gmail.com>
2021-05-30 23:47:40 -0300

    feat: ensure login present valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -20,10 +20,10 @@ const Input: React.FC<Props> = (props: Props) => {
     });
   };
   const getStatus = (): string => {
-    return "🔴";
+    return error ? "🔴" : "🟢";
   };
   const getTitle = (): string => {
-    return error;
+    return error || "Tudo certo";
   };
   return (
     <div className={Styles.inputWrap}>

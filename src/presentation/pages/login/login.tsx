972e3ada6a244f381c220df5cc82bfeb3b1d9db1
matheus <matheus.xmaz10@gmail.com>
2021-06-01 22:34:00 -0300

    feat: prevent login to call submit more than once

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -32,6 +32,9 @@ export default function Login({ validation, authentication }: Props) {
     event: React.FormEvent<HTMLFormElement>
   ): Promise<void> => {
     event.preventDefault();
+    if (state.isLoading) {
+      return;
+    }
     setState({
       ...state,
       isLoading: true,

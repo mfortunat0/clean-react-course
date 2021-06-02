aa77e29e99ad04293c677d92a58c4e3aa17936fd
matheus <matheus.xmaz10@gmail.com>
2021-06-01 22:58:35 -0300

    feat: ensure login add access token to localstorage on success

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -40,10 +40,11 @@ export default function Login({ validation, authentication }: Props) {
         ...state,
         isLoading: true,
       });
-      await authentication.auth({
+      const account = await authentication.auth({
         email: state.email,
         password: state.password,
       });
+      localStorage.setItem("accessToken", account.accessToken);
     } catch (error) {
       setState({
         ...state,

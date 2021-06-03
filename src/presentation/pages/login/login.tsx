50846abf9bc2c4b823e9872a644668abb0b6b57c
matheus <matheus.xmaz10@gmail.com>
2021-06-02 22:57:40 -0300

    feat: ensure login navigate to signup correctly

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Styles from "./login-styles.scss";
 import { Header, Footer, FormStatus, Input } from "@/presentation/components/";
 import Context from "@/presentation/context/form/form-context";
@@ -78,7 +79,9 @@ export default function Login({ validation, authentication }: Props) {
           >
             Entrar
           </button>
-          <span className={Styles.link}>Criar conta</span>
+          <Link data-testid="signup" to="/signup" className={Styles.link}>
+            Criar conta
+          </Link>
           <FormStatus />
         </form>
       </Context.Provider>

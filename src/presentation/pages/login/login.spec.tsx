dc57a20cf6282e929cb5ec0967479fda5e8a061f
matheus <matheus.xmaz10@gmail.com>
2021-06-02 23:01:31 -0300

    test: ensure login navigates to main page on success

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -23,7 +23,9 @@ type SutParams = {
   validationError: string;
 };
 
-const history = createMemoryHistory();
+const history = createMemoryHistory({
+  initialEntries: ["/login"],
+});
 
 const makeSut = (params?: SutParams): SutType => {
   const validationStub = new ValidationStub();
@@ -190,6 +192,8 @@ describe("Login Component", () => {
       "accessToken",
       authenticationSpy.account.accessToken
     );
+    expect(history.length).toBe(1);
+    expect(history.location.pathname).toBe("/");
   });
 
   test("Should go to signup page", () => {
